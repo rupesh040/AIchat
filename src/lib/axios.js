e649d8123a6cbd5f08a0efb5968eb53e7eb7e229
Rupesh kumar <112106535+rupesh040@users.noreply.github.com>
2024-11-29 17:46:40 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-     baseURL: "http://localhost:5001/api",
+     baseURL: "https://cha-tbackend.vercel.app/api",
      withCredentials:true,
      }
 );
